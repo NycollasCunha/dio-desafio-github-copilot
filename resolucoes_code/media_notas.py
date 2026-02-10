f84242263ccf6cbb288de355e6d157e3f137ac03
Nycollas Cunha <88843363+NycollasCunha@users.noreply.github.com>
2026-02-10 18:01:50 +0000

    Foi criado o arquivo "media_notas.py" que calcula a média entre 3 notas

---
+++ resolucoes_code/media_notas.py
@@ -0,0 +1,9 @@
+# Calcular a média de três notas
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+print(f"Média das notas: {media:.2f}")
