be7acde606edd6e77971fb06b8cd83188d8a9f84
Nycollas Cunha <88843363+NycollasCunha@users.noreply.github.com>
2026-02-10 17:53:54 +0000

    Criado o arquivo "repete_texto.py"

---
+++ resolucoes_code/repete_texto.py
@@ -0,0 +1,8 @@
+# Solicitar uma string e um número inteiro, depois retornar a string repetida
+
+texto = input("Digite uma string: ")
+numero = int(input("Digite quantas vezes repetir: "))
+
+resultado = texto * numero
+
+print(f"Resultado: {resultado}")
