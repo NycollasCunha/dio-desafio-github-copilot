dac3b8e8d4833a9a195cc5dffe00d41a362bfbe7
Nycollas Cunha <88843363+NycollasCunha@users.noreply.github.com>
2026-02-10 17:50:04 +0000

    Criado o arquivo "concat_dados.py" que recebe dois dados diferentes e os concatena

---
+++ resolucoes_code/concat_dados.py
@@ -0,0 +1,7 @@
+# Vamos receber dois dados diferentes do usuário e concatena-los
+dado1 = input("Digite o primeiro dado: ")
+dado2 = input("Digite o segundo dado: ")
+
+resultado = dado1 + dado2
+
+print(f"Concatenação: {resultado}")
